5073a7d0bb77bd261139c4be9bfa739de576c3e5
Igor Nagano <igornagano@hotmail.com>
2018-03-14 22:42:32 -0300

    CORS
    Habilitando o CORS na API

--- API/libs/middlewares.js
+++ API/libs/middlewares.js
@@ -4,9 +4,28 @@ var bodyParser = require('body-parser');
 module.exports = app => {
 	app.set("json spaces", 4);
 	app.use(bodyParser.json());
-	//app.use(app.auth.initialize());
+	app.use(app.auth.initialize());
 	app.use((req, res, next) => {
 		delete req.body.id;
 		next();
 	})
+	// Add headers
+	app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 };
